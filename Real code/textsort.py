05490adb75dda7f4a7669e93bdb0737fac0ed17e
theBarberThief <gabisonlouis@gmail.com>
2020-07-07 17:01:57 +0800

    fixed

--- Real code/textsort.py
+++ Real code/textsort.py
@@ -5,23 +5,17 @@ o = codecs.open('Output.txt', "w+", encoding='utf-8')
 text = f.readlines()
 count = 0
 for line in text:
+
     myList = []
     word = ""
     charCount = 0
-    for char in line:
-        if char == '.':
-            print("WOWOWOW")
-        if char != (' ' or '(' or ')' or '.' or ','):
-            word += char
-            charCount += 1
-
-        # if last letter
-        if charCount == len(line) - 1:
-            myList.append(word)
+    spaceCounter = 0
+    for letter in line:
+        if letter.isalnum():
+            word += letter
         else:
             myList.append(word)
             word = ""
-            charCount += 1
     final = ""
     for element in myList:
         final += element
