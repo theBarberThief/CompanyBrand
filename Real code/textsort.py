2ca2402409728a2e3b52ed65ce6431230ec5f54a
Jeffrey Kang <The4thStranger@gmail.com>
2020-07-10 12:12:21 +0800

    bruh

--- Real code/textsort.py
+++ Real code/textsort.py
@@ -1,23 +1,23 @@
 import codecs
+def OGList():
+    f = codecs.open('CompanyEN.txt', "r", encoding='utf-8')
+    o = codecs.open('Output.txt', "w+", encoding='utf-8')
+    text = f.readlines()
+    finalList = []
+    for line in text:
 
-f = codecs.open('CompanyEN.txt', "r", encoding='utf-8')
-o = codecs.open('Output.txt', "w+", encoding='utf-8')
-text = f.readlines()
-count = 0
-for line in text:
+        myList = []
+        word = ""
+        for letter in line:
+            if letter.isalnum():
+                word += letter
+            else:
+                myList.append(word)
+                word = ""
+        final = ""
+        for element in myList:
+            final += element + " "
 
-    myList = []
-    word = ""
-    charCount = 0
-    spaceCounter = 0
-    for letter in line:
-        if letter.isalnum():
-            word += letter
-        else:
-            myList.append(word)
-            word = ""
-    final = ""
-    for element in myList:
-        final += element
-
-    print(final)
+        print(final)
+        finalList.append(myList)
+    return finalList
