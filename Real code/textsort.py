f846abaef050551e62dd6640c27873b5a508d150
Jeffrey Kang <The4thStranger@gmail.com>
2020-07-10 16:10:01 +0800

    bruhyuh

--- Real code/textsort.py
+++ Real code/textsort.py
@@ -1,7 +1,7 @@
 import codecs
+import nltk
 def OGList():
     f = codecs.open('CompanyEN.txt', "r", encoding='utf-8')
-    o = codecs.open('Output.txt', "w+", encoding='utf-8')
     text = f.readlines()
     finalList = []
     for line in text:
@@ -21,3 +21,28 @@ def OGList():
         print(final)
         finalList.append(myList)
     return finalList
+
+def listToString(list):
+    str = ""
+    for i in list:
+        str = str + i + " "
+    return str
+
+#shit
+def returnNouns(str):
+    Nouns = []
+    sentence = nltk.word_tokenize(str)
+    tagged = nltk.pos_tag(sentence)
+    print(tagged)
+    for word,pos in tagged:
+
+        if (pos == 'NNP' or pos == 'NNPS' ):
+            Nouns.append(word)
+
+    print(listToString(Nouns))
+    return Nouns
+
+
+#def wordNumbers (List)
+returnNouns("Jackson went to China on a rainy day!")
+
